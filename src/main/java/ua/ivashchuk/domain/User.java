3dfd3c24b51e1ca01156e4f1c30ce88555e36de6
Java-legioner <ivashchuk.serhii@gmail.com>
2021-07-29 15:18:22 +0300

    Initial commit

---
+++ src/main/java/ua/ivashchuk/domain/User.java
@@ -0,0 +1,10 @@
+package ua.ivashchuk.domain;
+
+public class User {
+    private Integer id;
+    private String username;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean active;
+}
